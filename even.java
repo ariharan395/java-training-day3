c30710391934487a05a4dd29f5daa31a215309bd
ariharan395 <117897810+ariharan395@users.noreply.github.com>
2023-07-12 12:18:38 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class even
+{
+    public static void main(String[] args)
+    {
+//        Scanner sc = new Scanner(System.in);
+        even();
+    }
+
+    static void even()
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a % 2 == 0)
+        {
+            System.out.println("Even number");
+        }
+        else
+        {
+            System.out.println("Odd number");
+        }
+    }
+}
